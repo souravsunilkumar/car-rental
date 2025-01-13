d21cf388b193d0029a6107f3b2f34c0d429bebdb
jishnurameshc <jishnurameshc@gmail.com>
2025-01-13 06:12:33 +0000

    update maintainace doc

--- rental/rental/doctype/maintanance/maintanance.py
+++ rental/rental/doctype/maintanance/maintanance.py
@@ -1,9 +1,10 @@
 # Copyright (c) 2025, sourav and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
 from frappe.model.document import Document
-
+from frappe.utils import today, add_days, getdate
 
 class Maintanance(Document):
-	pass
+    pass
+
