75cca3fd79570b9f4bdaedb44221d2fcff76e0f0
souravsunilkumar <souravsunilkumar4@gmail.com>
2025-01-13 08:41:49 +0000

    report

--- rental/rental/doctype/car/car.py
+++ rental/rental/doctype/car/car.py
@@ -1,9 +1,24 @@
 # Copyright (c) 2025, sourav and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
 from frappe.model.document import Document
 
 
 class Car(Document):
-	pass
+    def validate(self):
+        self.update_booking_status()
+
+    def update_booking_status(self):
+        
+        if self.availability == "Available":
+            booking = frappe.get_all(
+                "Booking",
+                filters={"car": self.name, "status": "Rented"},
+                fields=["name"],
+                limit=1,
+            )
+            if booking:
+                booking_doc = frappe.get_doc("Booking", booking[0].name)
+                booking_doc.status = "Returned"
+                booking_doc.save()
---
+++ rental/rental/report/reminder/reminder.py
@@ -0,0 +1,56 @@
+# Copyright (c) 2025, sourav and contributors
+# For license information, please see license.txt
+
+import frappe
+def execute(filters=None):
+    
+    columns = [
+        {"fieldname": "car_name", "label": "Car Name", "fieldtype": "Data"},
+        {"fieldname": "model_name", "label": "Model Name", "fieldtype": "Data"},
+        {"fieldname": "insurance_expiry_date", "label": "Insurance Expiry Date", "fieldtype": "Date"},
+        {"fieldname": "status", "label": "Maintenance Status", "fieldtype": "Select", "options": "Pending\nDone"},
+        {"fieldname": "maintanance_date", "label": "Maintenance Date", "fieldtype": "Date"},
+        {"fieldname": "type", "label": "Maintenance Type", "fieldtype": "Data"},
+    ]
+
+    
+    cars = frappe.get_all(
+        "Car",
+        fields=["name", "car_name", "model_name", "insurance_expiry_date"],
+    )
+
+    
+    maintenance = frappe.get_all(
+        "Maintanance",
+        fields=["car", "maintanance_date", "status", "type"],
+    )
+
+    
+    data = []
+    for car in cars:
+        
+        car_maintenance = [m for m in maintenance if m.get("car") == car.get("name")]
+
+        if car_maintenance:
+            
+            for record in car_maintenance:
+                data.append({
+                    "car_name": car["car_name"],
+                    "model_name": car["model_name"],
+                    "insurance_expiry_date": car["insurance_expiry_date"],
+                    "status": record["status"],
+                    "maintanance_date": record["maintanance_date"],
+                    "type": record["type"],
+                })
+        else:
+           
+            data.append({
+                "car_name": car["car_name"],
+                "model_name": car["model_name"],
+                "insurance_expiry_date": car["insurance_expiry_date"],
+                "status": None,
+                "maintanance_date": None,
+                "type": None,
+            })
+
+    return columns, data
